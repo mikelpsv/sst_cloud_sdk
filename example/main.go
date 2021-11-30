04c18e0c38f8df230196e8a0bbc811d3b3225184
Mike <mikelpsv@gmail.com>
2021-11-30 12:57:39 +0300

    example added

---
+++ example/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	sst "github.com/mikelpsv/sst_cloud_sdk"
+)
+
+const (
+	USERNAME = ""
+	PASSWORD = ""
+	YOUR_MAIL = ""
+
+)
+
+func main() {
+	s := new(sst.Session)
+
+	// Авторизация
+	_, err := s.Login(sst.LoginRequest{Username: USERNAME, Password: PASSWORD, Email: YOUR_MAIL})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer s.Logout()
+
+	// Чтение профиля пользователя
+	ui, err := s.UserInfo()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ui)
+
+	// Чтение домовладений текущего аккаунтв
+	houses, err := s.GetHouses()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, h := range houses {
+		// домовладение по id
+		house, err := s.GetHouse(h.Id)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(house)
+
+		// устройства
+		devs, err := s.GetDevices(h.Id)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		for _, d := range devs {
+			dev, err := s.GetDevice(h.Id, d.Id)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			conf, _ := dev.ReadConfigThermostat()
+			fmt.Println(dev, conf)
+
+			//s.SetDeviceStatus(&d, sst.DEVICE_STATUS_ON)
+		}
+	}
+
+}
